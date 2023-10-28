71c0d786dd6c8b1789869ba6cd9b490f71e466c7
David <mrriptidemc@gmail.com>
2023-10-28 12:08:16 -0400

    actual init commit

---
+++ app/search/page.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { useLazyQuery } from "@apollo/client";
+import { Button, Container, FormControl, InputLabel, MenuItem, TextField, Unstable_Grid2 as Grid } from "@mui/material";
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useRef, useState } from "react";
+import { gql } from "../../src/__generated__";
+import ClassDisplay from "../lib/ClassDisplay";
+import { SectionData, useTerms } from "../lib/Common";
+import "./Search.css";
+
+export interface SnackbarMessage {
+  message: string;
+  key: number;
+}
+
+export default function Page() {
+  const [items, setItems] = useState<SectionData[]>([]);
+  const searchRef = useRef<HTMLInputElement | null>(null);
+  
+  let class_numbers: Array<number> = [];
+
+  const search: () => {
+
+  }
+
+  return (
+      <>
+      <Grid container spacing={2}>
+        <Grid>
+          <TextField name="search" label="Search Query" inputRef={searchRef} variant="outlined" size="small" margin="none" />
+        </Grid>
+        <Grid>
+          <Button onClick={() => loadClasses()} variant="contained" size="medium">Search</Button>
+        </Grid>
+      </Grid>
+      <Container id="resultsContainer">
+        { data ? data.classes.map((item) =>
+          <ClassDisplay key={item.classNumber} classInfo={item}></ClassDisplay>
+        ) : null }
+        
+      </Container>
+      </>
+    );
+}
