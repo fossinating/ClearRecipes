fc7b5893858804ff6dad50774df1aa62d125e26f
krohan03 <106789798+krohan03@users.noreply.github.com>
2023-10-29 11:14:50 -0400

    Fixed home page display

--- app/api/home/route.ts
+++ app/api/home/route.ts
@@ -6,26 +6,23 @@ export const runtime = 'edge';
  
 export async function GET(req: NextRequest) {
     const session = await auth()
-    if (session && session?.user && session.user?.email) {
-        const recipes = await db.query.recipes.findMany({
-            limit: 3,
-            where: (recipes, {eq}) => eq(recipes.ownerID, session.user.id),
-            with: {
-                recipesToIngredients: {
-                    with: {
-                        ingredient: true
-                    }
+    const recipes = await db.query.recipes.findMany({
+        limit: 3,
+        with: {
+            recipesToIngredients: {
+                with: {
+                    ingredient: true
                 }
             }
-        })
+        }
+    })
 
-        return NextResponse.json({ 
-            recipes: recipes.map((recipeData) => {
-                return ({
-                    ...recipeData,
-                    ingredients: recipeData.recipesToIngredients.map((recipeToIngrient) => ({name: recipeToIngrient.ingredient, quantity: recipeToIngrient.quantity}))
-                })
-            }).map(({recipesToIngredients, ...rest}) => rest)
-        }, {status: 200});
-    }
+    return NextResponse.json({ 
+        recipes: recipes.map((recipeData) => {
+            return ({
+                ...recipeData,
+                ingredients: recipeData.recipesToIngredients.map((recipeToIngrient) => ({name: recipeToIngrient.ingredient, quantity: recipeToIngrient.quantity}))
+            })
+        }).map(({recipesToIngredients, ...rest}) => rest)
+    }, {status: 200});
 }
